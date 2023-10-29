bed7c9c4f560acd07e8babb4949fa7d7866154f6
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-10-29 19:10:18 +0300

    add nonmutating setter

--- Sources/FileManagerWrapper/FileManagerWrapper.swift
+++ Sources/FileManagerWrapper/FileManagerWrapper.swift
@@ -8,7 +8,7 @@ public struct FileManagerWrapper<T: Codable>: DynamicProperty {
     
     public var wrappedValue: T? {
         get { value }
-        set { value = newValue }
+        nonmutating set { value = newValue }
     }
     
     public var projectedValue: T? {
